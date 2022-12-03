ee2acc7fea87c8f42cc4c25a175ccfb2e9a593c3
Gill Erick <ogayogill95@gmail.com>
2022-12-03 23:33:35 +0300

    Implement command execution

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 )
 
 // docker run <container> cmd args
@@ -21,4 +23,17 @@ func main() {
 func run() {
 	// We print out all arguments starting from the second one
 	fmt.Printf("running %v\n", os.Args[2:])
+
+	// Here, we are running command 2, and optionally 3 onwards
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	// We are running the command we set up on line 28
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Command failed with error %v", err)
+		return
+	}
+
 }
